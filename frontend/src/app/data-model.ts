c6f6ca149735309dea2688599d08b8b07b7f602f
dannymcwaves <dannymcwaves96@gmail.com>
2017-12-01 12:04:15 +0000

    model of the data for the lists to be returned

---
+++ frontend/src/app/data-model.ts
@@ -0,0 +1,10 @@
+
+export const ListData:string[] = [
+  'go to the mall',
+  'Really Smart',
+  'Super Flexible',
+  'Super Hot',
+  'Weather Changer',
+  'Change me clothes',
+  'be super flexible'
+];
