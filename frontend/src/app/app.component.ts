7fd8fa07af1413eec141a8a31bd101939681444e
dannymcwaves <dannymcwaves96@gmail.com>
2017-12-11 12:24:41 +0000

    app component module updated to add routes

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+
+export class AppComponent{
+  title = 'app';
+}
